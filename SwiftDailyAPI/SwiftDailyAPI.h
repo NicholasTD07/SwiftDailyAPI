1615b1fb85246acb3138a7768997d2c72faf8368
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-25 10:26:13 +1000

    Initial Commit
    Setup project structure and also Carthage as dependency manager.

---
+++ SwiftDailyAPI/SwiftDailyAPI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDailyAPI.h
+//  SwiftDailyAPI
+//
+//  Created by Nicholas Tian on 25/05/2015.
+//  Copyright (c) 2015 nickTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDailyAPI.
+FOUNDATION_EXPORT double SwiftDailyAPIVersionNumber;
+
+//! Project version string for SwiftDailyAPI.
+FOUNDATION_EXPORT const unsigned char SwiftDailyAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDailyAPI/PublicHeader.h>
+
+
