8d9b603732e3243c7e590b6972b805c04227c348
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-25 21:21:50 +1000

    Change to import Foundation.h in SwiftDailyAPI.h

--- SwiftDailyAPI/SwiftDailyAPI.h
+++ SwiftDailyAPI/SwiftDailyAPI.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 nickTD. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftDailyAPI.
 FOUNDATION_EXPORT double SwiftDailyAPIVersionNumber;
