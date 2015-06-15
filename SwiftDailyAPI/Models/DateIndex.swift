f5d24b7b128a0ad06948934677c1d01da9522f2a
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-15 21:40:29 +1000

    Add `TimelineCollection` and `DateIndex`

---
+++ SwiftDailyAPI/Models/DateIndex.swift
@@ -0,0 +1,51 @@
+//
+//  DateIndex.swift
+//  SwiftDailyAPI
+//
+//  Created by Nicholas Tian on 15/06/2015.
+//  Copyright © 2015 nickTD. All rights reserved.
+//
+
+import Foundation
+
+public struct DateIndex {
+  let date: NSDate
+  private var calendar = NSCalendar.currentCalendar()
+
+  public init(_ date: NSDate) {
+    self.date = calendar.startOfDayForDate(date)
+  }
+}
+
+public func == (lhs: DateIndex, rhs: DateIndex) -> Bool {
+  return lhs.date == rhs.date
+}
+
+extension DateIndex: Equatable { }
+
+extension DateIndex: Hashable {
+  public var hashValue: Int {
+    return date.hashValue
+  }
+}
+
+extension DateIndex: RandomAccessIndexType {
+  public func successor() -> DateIndex {
+    let nextDay = calendar.dateByAddingUnit(.Day, value: 1, toDate: date, options: [])!
+    return DateIndex(nextDay)
+  }
+
+  public func predecessor() -> DateIndex {
+    let previousDay = calendar.dateByAddingUnit(.Day, value: -1, toDate: date, options: [])!
+    return DateIndex(previousDay)
+  }
+
+  public func advancedBy(n: Int) -> DateIndex {
+    let advancedDate = calendar.dateByAddingUnit(.Day, value: n, toDate: date, options: [])!
+    return DateIndex(advancedDate)
+  }
+
+  public func distanceTo(other: DateIndex) -> Int {
+    return calendar.components(.Day, fromDate: date, toDate: other.date, options: []).day
+  }
+}
---
+++ SwiftDailyAPI/Models/TimelineCollection.swift
@@ -0,0 +1,69 @@
+//
+//  TimelineCollection.swift
+//  SwiftDailyAPI
+//
+//  Created by Nicholas Tian on 15/06/2015.
+//  Copyright © 2015 nickTD. All rights reserved.
+//
+
+import Foundation
+
+public struct TimelineCollection<T> {
+  var storage: [DateIndex: T] = [:]
+
+  var startIndex: DateIndex
+  var endIndex: DateIndex
+
+  public init(startDate: NSDate, endDate: NSDate) {
+    startIndex = DateIndex(startDate)
+    endIndex = DateIndex(endDate)
+  }
+
+  // MARK: Subscrit with `DateIndex`
+  public subscript (i: DateIndex) -> T? {
+    get {
+      return storage[i]
+    }
+    set {
+      storage[i] = newValue
+
+      // The `TimelineCollection` expands dynamically to include the earliest and latest dates.
+      // To implement this functionality, any time a new date-image pair is stored using this
+      // subscript, we check to see if the date is before the `startIndex` or after the `endIndex`.
+      if i.date.compare(startIndex.date) == .OrderedAscending {
+        startIndex = i
+      }
+
+      let endIndexComparison = i.date.compare(endIndex.date)
+      if endIndexComparison == .OrderedDescending || endIndexComparison == .OrderedSame {
+        // The `endIndex` is always one past the end so that iteration and `count` know when
+        // to stop, so compute the successor before storing the new `endIndex`.
+        endIndex = i.successor()
+      }
+    }
+  }
+}
+
+// MARK: Subscript with `NSDate`
+extension TimelineCollection {
+  public subscript (date: NSDate) -> T? {
+    get {
+      return self[DateIndex(date)]
+    }
+    set {
+      self[DateIndex(date)] = newValue
+    }
+  }
+}
+
+// MARK: Subscript with `Int`
+extension TimelineCollection {
+  public subscript (i: Int) -> T? {
+    get {
+      return self[startIndex.advancedBy(i)]
+    }
+    set {
+      self[startIndex.advancedBy(i)] = newValue
+    }
+  }
+}
