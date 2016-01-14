1271d0a4a7c453f9c692e1ecbaa39a5009c4feca
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-01-14 10:27:55 +0800

    Almost fully tested DateIndex

--- SwiftDailyAPITests/Specs/DateIndexSpec.swift
+++ SwiftDailyAPITests/Specs/DateIndexSpec.swift
@@ -15,12 +15,26 @@ class DateIndexSpec: QuickSpec {
     let dateFormatter = NSDateFormatter()
     dateFormatter.dateFormat = DailyConstants.dateFormat
 
-    it("has the correct date") {
-      let today = NSDate()
-      let dateIndex = DateIndex(today)
+    let todaysDate = NSDate()
+    let dateBeforeToday = NSDate().dayBefore()
 
-      expect(dateFormatter.stringFromDate(today))
-        .to(equal(dateFormatter.stringFromDate(dateIndex.date)))
+    let today = DateIndex(todaysDate)
+    let tomorrow = today.successor()
+    let yesterday = today.predecessor()
+
+
+    it("has the correct date") {
+      expect(dateFormatter.stringFromDate(todaysDate))
+        .to(equal(dateFormatter.stringFromDate(today.date)))
+    }
+
+    it("yesterday is not today") {
+      expect(today == yesterday).toNot(beTrue())
+      expect(today.hashValue == yesterday.hashValue).toNot(beTrue())
+    }
+
+    fit("tomorrow is one day later") {
+      expect(today.distanceTo(tomorrow)).to(equal(1))
     }
   }
 }
