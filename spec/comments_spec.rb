05d013b50bd24683b1efb332845c725635811342
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-01-20 10:06:32 +0800

    Expect short and long comments to have Comments in SDAPI

---
+++ spec/comments_spec.rb
@@ -0,0 +1,36 @@
+describe 'In a news\'s short comments' do
+  before(:all) do
+    get '/news/7190794/short-comments'
+  end
+
+  it 'has comments' do
+    expect_json_types(
+      'comments.*',
+      id: :int,
+      author: :string,
+      content: :string,
+      likes: :int,
+      time: :int, # timestamp
+      avatar: :string # url
+    )
+  end
+end
+
+
+describe 'In a news\'s long comments' do
+  before(:all) do
+    get '/news/7190794/long-comments'
+  end
+
+  it 'has comments' do
+    expect_json_types(
+      'comments.*',
+      id: :int,
+      author: :string,
+      content: :string,
+      likes: :int,
+      time: :int, # timestamp
+      avatar: :string # url
+    )
+  end
+end
