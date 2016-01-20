e9f9a3dea2cfddafaa5f03608f0ed220be40e604
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-01-20 10:09:22 +0800

    Extract shared `it` block in comments_spec using RSpec.shared_examples

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -1,8 +1,4 @@
-describe 'In a news\'s short comments' do
-  before(:all) do
-    get '/news/7190794/short-comments'
-  end
-
+RSpec.shared_examples 'has comments' do
   it 'has comments' do
     expect_json_types(
       'comments.*',
@@ -15,6 +11,13 @@ describe 'In a news\'s short comments' do
     )
   end
 end
+describe 'In a news\'s short comments' do
+  before(:all) do
+    get '/news/7190794/short-comments'
+  end
+
+  include_examples 'has comments'
+end
 
 
 describe 'In a news\'s long comments' do
@@ -22,15 +25,5 @@ describe 'In a news\'s long comments' do
     get '/news/7190794/long-comments'
   end
 
-  it 'has comments' do
-    expect_json_types(
-      'comments.*',
-      id: :int,
-      author: :string,
-      content: :string,
-      likes: :int,
-      time: :int, # timestamp
-      avatar: :string # url
-    )
-  end
+  include_examples 'has comments'
 end
